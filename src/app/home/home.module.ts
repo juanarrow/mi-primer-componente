2c0fce008dc6e2772c0b168301d08d71d0d018e9
Juan A. García <juan.garcia@aliqindoi.com>
2024-10-02 13:46:24 +0200

    some changes

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -5,6 +5,7 @@ import { FormsModule } from '@angular/forms';
 import { HomePage } from './home.page';
 
 import { HomePageRoutingModule } from './home-routing.module';
+import { PersonalCardComponent } from '../personal-card/personal-card.component';
 
 
 @NgModule({
@@ -14,6 +15,6 @@ import { HomePageRoutingModule } from './home-routing.module';
     IonicModule,
     HomePageRoutingModule
   ],
-  declarations: [HomePage]
+  declarations: [HomePage, PersonalCardComponent]
 })
 export class HomePageModule {}
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,10 @@
 import { Component } from '@angular/core';
 
+export interface Person{
+  name:string,
+  surname:string,
+  age:number
+}
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -7,6 +12,30 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  people:Person[] = [];
+  constructor() {
+    this.people.push({
+      name:"Juan A.",
+      surname:"García Gómez",
+      age:47
+    });
+    this.people.push({
+      name:"Alejandro.",
+      surname:"García Gómez",
+      age:46
+    });
+
+    this.people.push({
+      name:"Juan",
+      surname:"García Valencia",
+      age:5
+    });
+
+    this.people.push({
+      name:"María del Mar",
+      surname:"Valencia Valencia",
+      age:47
+    });
+  }
 
 }
